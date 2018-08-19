8147d90a4ff39a6a645e33d55235da01c1082077
eit90123 <760390101@qq.com>
2018-08-19 22:49:14 +0800

    1

--- keepEmptyDir.sh
+++ keepEmptyDir.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 ###create .ingore dummy file for copying to empty folder
-touch .gitingore
+touch .gitignore
 
 ###use find the files and folders list 
 for i in `find` ; do
 	if [ -e $i ] ; then
 		if [ ! "`ls -A $i`" ] ; then
 			echo $i is empty folder
-			cp  .gitingore $i
+			cp  .gitignore $i
 		fi
 	fi
 done
